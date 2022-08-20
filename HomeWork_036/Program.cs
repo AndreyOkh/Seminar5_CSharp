d53719528922ab5dc0af3f368cf0181dd4b5d2fd
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-20 21:41:05 +1000

    Найдите сумму элементов, стоящих на нечётных позициях массива.

--- HomeWork_036/Program.cs
+++ HomeWork_036/Program.cs
@@ -2,3 +2,34 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int sizeArray = 10;
+int[] numbers = new int[sizeArray];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sum = 0;
+int i = 0;
+while (i < numbers.Length)
+{
+    sum += numbers[i];
+    i += 2;
+}
+
+Console.WriteLine($"Сумма чисел стоящих на нечетных позициях равна {sum}.");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
