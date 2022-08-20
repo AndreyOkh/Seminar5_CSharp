4ac98a8a18d4dfbb2d8ef4f7d537555b2d263277
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-20 21:35:57 +1000

    Напишите программу, которая покажет количество чётных чисел в массиве.

--- HomeWork_034/Program.cs
+++ HomeWork_034/Program.cs
@@ -2,3 +2,32 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int sizeArray = 10;
+int[] numbers = new int[sizeArray];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int countEvenNumbers = 0;
+foreach (int number in numbers)
+{
+    if(number % 2 == 0) countEvenNumbers++;
+}
+
+Console.WriteLine($"В массиве {countEvenNumbers} четных чисел.");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
